fb3db4147b75b5322f861ba707cfa059bf1b2adf
Yatin <yatinkathuria74@gmail.com>
2021-07-04 15:03:57 +0530

    programaticaly route to product page

--- next-routing/pages/index.js
+++ next-routing/pages/index.js
@@ -1,6 +1,12 @@
 import Link from "next/link";
+import router from "next/router";
 
 function Home() {
+  const handleClick = () => {
+    console.log("Placing Your Order");
+    router.push("/product");
+  };
+
   return (
     <div>
       <h2>Home Page</h2>
@@ -10,6 +16,7 @@ function Home() {
       <Link href="/product">
         <a>Products</a>
       </Link>
+      <button onClick={handleClick}>Place Order</button>
     </div>
   );
 }
