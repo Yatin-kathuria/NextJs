ccbd1efea9c6b6779249a47b8938847670784947
Yatin <yatinkathuria74@gmail.com>
2021-07-04 14:46:35 +0530

    learn Link component for routing by button

--- next-routing/pages/index.js
+++ next-routing/pages/index.js
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 function Home() {
-  return <h2>Home Page</h2>;
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
+      <Link href="/product">
+        <a>Products</a>
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
